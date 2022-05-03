2966925330753b38efc9e540a3b90bfec89a2d01
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-05-03 20:39:49 +0100

    completes few more

--- models/park.js
+++ models/park.js
@@ -4,4 +4,31 @@ const Park = function (parkName, ticketPrice, dinos) {
   this.dinos = dinos;
 };
 
+Park.prototype.addDino = function (dinoToAdd) {
+  this.dinos.push(dinoToAdd);
+};
+
+Park.prototype.deleteDino = function (dinoToDelete) {
+  let filteredArray = this.dinos.filter((dino) => dino !== dinoToDelete);
+  this.dinos = filteredArray;
+};
+
+Park.prototype.GetDinoInfluencer = function () {
+  this.dinos.sort(function (a, b) {
+    return a.guestsAttractedPerDay - b.guestsAttractedPerDay;
+  });
+  let dinoInfluencer = this.dinos.at(-1);
+  return dinoInfluencer;
+};
+
+Park.prototype.findSpecies = function (species) {
+  let dinoBySpecie = [];
+  for (let dino of this.dinos) {
+    if (dino.species === species) {
+      dinoBySpecie.push(dino);
+    }
+  }
+  return dinoBySpecie;
+};
+
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -7,7 +7,7 @@ describe("Park", function () {
   let dinos_list = [];
   beforeEach(function () {
     dino1 = new Dinosaur("Garudimimus", "Omnivorous", 150);
-    dino2 = new Dinosaur("Agilisaurus", "Herbivorous", 350);
+    dino2 = new Dinosaur("Garudimimus", "Herbivorous", 350);
     dino3 = new Dinosaur("Argentinosaurus", "Herbivorous", 50);
     dino4 = new Dinosaur("Tyrannosaurus", "Carnivorous", 1000);
     dinos_list = [dino1, dino2, dino3, dino4];
@@ -29,13 +29,30 @@ describe("Park", function () {
     assert.strictEqual(actual, 4);
   });
 
-  it("should be able to add a dinosaur to its collection");
+  it("should be able to add a dinosaur to its collection", function () {
+    const dinoToAdd = new Dinosaur("Philoceratops", "Omnivorous", 38);
+    park.addDino(dinoToAdd);
+    const actual = park.dinos.length;
+    assert.strictEqual(actual, 5);
+  });
 
-  it("should be able to remove a dinosaur from its collection");
+  it("should be able to remove a dinosaur from its collection", function () {
+    park.deleteDino(dino4);
+    const actual = park.dinos.length;
+    assert.strictEqual(actual, 3);
+  });
 
-  it("should be able to find the dinosaur that attracts the most visitors");
+  it("should be able to find the dinosaur that attracts the most visitors", function () {
+    const dinoInfluencer = park.GetDinoInfluencer();
+    const actual = dinoInfluencer.species;
+    assert.strictEqual(actual, "Tyrannosaurus");
+  });
 
-  it("should be able to find all dinosaurs of a particular species");
+  it("should be able to find all dinosaurs of a particular species", function () {
+    const filterBySpecie = park.findSpecies("Garudimimus");
+    const actual = filterBySpecie.length;
+    assert.strictEqual(actual, 2);
+  });
 
   it("should be able to calculate the total number of visitors per day");
 
